7f8d6d149d94e4d2efad0ca4d10638b52e2b9ecc
Marwankhairallah <marwan.khairallah@u-psud.fr>
2024-12-03 19:21:33 +0100

    share buttons

--- static/js/share.js
+++ static/js/share.js
@@ -1,36 +1,32 @@
-// Fonction pour partager sur WhatsApp
 const shareOnWhatsApp = () => {
     const quoteText = document.getElementById("quote-container").innerText;
     const url = `https://wa.me/?text=${encodeURIComponent(quoteText)}`;
     window.open(url, "_blank");
 };
 
-// Fonction pour partager sur Twitter
 const shareOnTwitter = () => {
     const quoteText = document.getElementById("quote-container").innerText;
     const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteText)}`;
     window.open(url, "_blank");
 };
 
-// Fonction pour partager sur Facebook
 const shareOnFacebook = () => {
     const quoteText = document.getElementById("quote-container").innerText;
     const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-        "https://your-site-url.com"
+        "https://quote-site-b9024.web.app/"
     )}&quote=${encodeURIComponent(quoteText)}`;
     window.open(url, "_blank");
 };
 
-// Fonction pour partager sur LinkedIn
 const shareOnLinkedIn = () => {
     const quoteText = document.getElementById("quote-container").innerText;
     const url = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
-        "https://your-site-url.com"
+        "https://quote-site-b9024.web.app/"
     )}&title=Quote&summary=${encodeURIComponent(quoteText)}`;
     window.open(url, "_blank");
 };
 
-// Rendre les fonctions disponibles globalement
+// Expose les fonctions globalement
 window.shareOnWhatsApp = shareOnWhatsApp;
 window.shareOnTwitter = shareOnTwitter;
 window.shareOnFacebook = shareOnFacebook;
